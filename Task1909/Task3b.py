712c2919b19c0c1235a235a20d3a8dba3dbb51b5
dmitry.kharitonov <dmitrykharitonov@yandex.ru>
2017-10-23 22:04:53 +0300

    %verlet%

---
+++ Task1909/Task3b.py
@@ -0,0 +1,386 @@
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
+import matplotlib.pyplot as plt
+import numpy as np
+
+eps=10 ** -6
+def intersect2(A1,A2,A3,A4):
+    val1=(A1[0]-A2[0])*(A4[2]-A3[2])-(A1[2]-A2[2])*(A4[0]-A3[0])
+    val2=(A1[0]-A2[0])*(A4[1]-A3[1])-(A1[1]-A2[1])*(A4[0]-A3[0])
+    val3=(A1[1]-A2[1])*(A4[2]-A3[2])-(A1[2]-A2[2])*(A4[1]-A3[1])
+    if (val1==0) and (val2==0) and (val3==0):
+        val4=(A4[0]-A2[0])*(A1[2]-A2[2])-(A4[2]-A2[2])*(A1[0]-A2[0])
+        val5=(A4[1]-A2[1])*(A1[2]-A2[2])-(A4[2]-A2[2])*(A1[1]-A2[1])
+        val6=(A4[0]-A2[0])*(A1[1]-A2[1])-(A4[1]-A2[1])*(A1[0]-A2[0])
+        inp1=val4 ** 2 + val5 ** 2 + val6 ** 2
+
+        val7=(A3[0]-A2[0])*(A1[2]-A2[2])-(A3[2]-A2[2])*(A1[0]-A2[0])
+        val8=(A3[1]-A2[1])*(A1[2]-A2[2])-(A3[2]-A2[2])*(A1[1]-A2[1])
+        val9=(A3[0]-A2[0])*(A1[1]-A2[1])-(A3[1]-A2[1])*(A1[0]-A2[0])
+        inp2=val7 ** 2 + val8 ** 2 + val9 ** 2
+
+        if (A1[0]!=A2[0]):
+            u=(A4[0]-A2[0])/(A1[0]-A2[0])
+            v=(A3[0]-A2[0])/(A1[0]-A2[0])
+        elif (A1[1]!=A2[1]):
+            u=(A4[1]-A2[1])/(A1[1]-A2[1])
+            v=(A3[1]-A2[1])/(A1[1]-A2[1])
+        elif (A1[2]!=A2[2]):
+            u=(A4[2]-A2[2])/(A1[2]-A2[2])
+            v=(A3[2]-A2[2])/(A1[2]-A2[2])
+        else:
+            print("Точки совпадают")
+            return -5
+
+        if (inp2==inp1) and (inp2==0) and (((v>=0) and (v<=1)) or ((u>=0) and (u<=1)) or ((u>=1) and (v<=0)) or ((v>=1) and (u<=0))):
+            return 1
+        elif (inp1==0) and ((v>=0) and (v<=1)):
+            return 2
+        elif (inp2==0) and ((u>=0) and (u<=1)):
+            return 3
+        else:
+            return 0
+    else:
+        if (val1!=0):
+            u=((A3[0]-A2[0])*(A4[2]-A3[2])-(A3[2]-A2[2])*(A4[0]-A3[0]))/val1;
+            v=-((A3[0]-A2[0])*(A2[2]-A1[2])-(A3[2]-A2[2])*(A2[0]-A1[0]))/val1;
+        elif (val3!=0):
+            u=((A3[1]-A2[1])*(A4[2]-A3[2])-(A3[2]-A2[2])*(A4[1]-A3[1]))/val3;
+            v=-((A3[1]-A2[1])*(A2[2]-A1[2])-(A3[2]-A2[2])*(A2[1]-A1[1]))/val3;
+        else:
+            u=((A3[0]-A2[0])*(A4[1]-A3[1])-(A3[1]-A2[1])*(A4[0]-A3[0]))/val2;
+            v=-((A3[0]-A2[0])*(A2[1]-A1[1])-(A3[1]-A2[1])*(A2[0]-A1[0]))/val2;
+        A5=[0]*3;
+        for i in range(0,3):
+            A5[i]=A2[i]+(A1[i]-A2[i])*u;
+        val4=(A5[0]-A2[0])*(A1[2]-A2[2])-(A5[2]-A2[2])*(A1[0]-A2[0])
+        val5=(A5[1]-A2[1])*(A1[2]-A2[2])-(A5[2]-A2[2])*(A1[1]-A2[1])
+        val6=(A5[0]-A2[0])*(A1[1]-A2[1])-(A5[1]-A2[1])*(A1[0]-A2[0])
+        inp1=val4 ** 2 + val5 ** 2 + val6 ** 2
+
+        val7=(A5[0]-A3[0])*(A4[2]-A3[2])-(A5[2]-A3[2])*(A4[0]-A3[0])
+        val8=(A5[1]-A3[1])*(A4[2]-A3[2])-(A5[2]-A3[2])*(A4[1]-A3[1])
+        val9=(A5[0]-A3[0])*(A4[1]-A3[1])-(A5[1]-A3[1])*(A4[0]-A3[0])
+        inp2=val7 ** 2 + val8 ** 2 + val9 ** 2
+        if (inp2==inp1) and (inp2==0) and (((v>=0) and (v<=1)) and ((u>=0) and (u<=1))):
+            return 4
+        else:
+            return -1
+
+def deter(A1,A2,A3,A4):
+    return ((A4[0]-A1[0])*((A2[1]-A1[1])*(A3[2]-A1[2])-(A3[1]-A1[1])*(A2[2]-A1[2]))-
+            (A4[1]-A1[1])*((A2[0]-A1[0])*(A3[2]-A1[2])-(A3[0]-A1[0])*(A2[2]-A1[2]))+
+            (A4[2]-A1[2])*((A2[0]-A1[0])*(A3[1]-A1[1])-(A3[0]-A1[0])*(A2[1]-A1[1])))
+
+def internal(A1,A2,A3,A4):
+    val1=(A2[0]-A1[0])*(A3[1]-A1[1])-(A3[0]-A1[0])*(A2[1]-A1[1])
+    val2=(A2[0]-A1[0])*(A3[2]-A1[2])-(A3[0]-A1[0])*(A2[2]-A1[2])
+    val3=(A2[1]-A1[1])*(A3[2]-A1[2])-(A3[1]-A1[1])*(A2[2]-A1[2])
+    if (abs(val1)>eps):
+        u=((A4[0]-A1[0])*(A3[1]-A1[1])-(A3[0]-A1[0])*(A4[1]-A1[1]))/val1
+        v=-((A4[0]-A1[0])*(A2[1]-A1[1])-(A2[0]-A1[0])*(A4[1]-A1[1]))/val1
+    elif (abs(val2)>eps):
+        u=((A4[0]-A1[0])*(A3[2]-A1[2])-(A3[0]-A1[0])*(A4[2]-A1[2]))/val2
+        v=-((A4[0]-A1[0])*(A2[2]-A1[2])-(A2[0]-A1[0])*(A4[2]-A1[2]))/val2
+    elif (abs(val3)>eps):
+        u=((A4[1]-A1[1])*(A3[2]-A1[2])-(A3[1]-A1[1])*(A4[2]-A1[2]))/val3
+        v=-((A4[1]-A1[1])*(A2[2]-A1[2])-(A2[1]-A1[1])*(A4[2]-A1[2]))/val3
+    else:
+        print("Точки совпадают")
+        return -1
+    if (abs(u-0.5)<0.5+eps) and (abs(v-0.5)<0.5+eps):
+        return 1
+    else:
+        return 0
+
+def intersect3(A1,A2,A3,A4,A5):
+    return deter(A1,A2,A3,A4)/(deter(A1,A2,A3,A4)-deter(A1,A2,A3,A5));
+
+def dist(A1,A2):
+    sum=0
+    for i in range(0,3):
+        sum+= (A1[i]-A2[i]) ** 2
+    return sum ** 0.5
+
+def intersect(A1,A2,A3,A4,A5,A6):
+    if (dist(A1,A2)<eps) or (dist(A1,A3)<eps) or (dist(A3,A2)<eps) or (dist(A4,A5)<eps) or (dist(A4,A6)<eps) or (dist(A5,A6)<eps):
+        print("Точки совпадают")
+        return -1
+    val1=(A2[0]-A1[0])*(A3[1]-A1[1])-(A3[0]-A1[0])*(A2[1]-A1[1])
+    val2=(A2[0]-A1[0])*(A3[2]-A1[2])-(A3[0]-A1[0])*(A2[2]-A1[2])
+    val3=(A2[1]-A1[1])*(A3[2]-A1[2])-(A3[1]-A1[1])*(A2[2]-A1[2])
+    if (abs(val1)<eps) and (abs(val2)<eps) and (abs(val3)<eps):
+        print("Три точки лежат на одной прямой")
+        return -1
+    val1=(A5[0]-A4[0])*(A6[1]-A4[1])-(A6[0]-A4[0])*(A5[1]-A4[1])
+    val2=(A5[0]-A4[0])*(A6[2]-A4[2])-(A6[0]-A4[0])*(A5[2]-A4[2])
+    val3=(A5[1]-A4[1])*(A6[2]-A4[2])-(A6[1]-A4[1])*(A5[2]-A4[2])
+    if (abs(val1)<eps) and (abs(val2)<eps) and (abs(val3)<eps):
+        print("Три точки лежат на одной прямой")
+        return -1
+    isct=[0]*9;
+    isct[0]=intersect2(P1,P2,P4,P5)
+    isct[1]=intersect2(P1,P2,P4,P6)
+    isct[2]=intersect2(P1,P2,P6,P5)
+    isct[3]=intersect2(P1,P3,P4,P5)
+    isct[4]=intersect2(P1,P3,P4,P6)
+    isct[5]=intersect2(P1,P3,P6,P5)
+    isct[6]=intersect2(P2,P3,P4,P5)
+    isct[7]=intersect2(P2,P3,P4,P6)
+    isct[8]=intersect2(P2,P3,P6,P5)
+    for a in isct:
+        if a>0:
+            return 1
+        if a==-5:
+            return -1
+    mas1=[0]*3
+    mas2=[0]*3
+    mas1[0]=deter(A1,A2,A3,A4)
+    mas1[1]=deter(A1,A2,A3,A5)
+    mas1[2]=deter(A1,A2,A3,A6)
+    mas2[0]=deter(A4,A5,A6,A1)
+    mas2[1]=deter(A4,A5,A6,A2)
+    mas2[2]=deter(A4,A5,A6,A3)
+    if (abs(mas1[0])<eps):
+        if internal(A1,A2,A3,A4)==1:
+            return 1
+    if (abs(mas1[1])<eps):
+        if internal(A1,A2,A3,A5)==1:
+            return 1
+    if (abs(mas1[2])<eps):
+        if internal(A1,A2,A3,A6)==1:
+            return 1
+    if (abs(mas2[0])<eps):
+        if internal(A4,A5,A6,A1)==1:
+            return 1
+    if (abs(mas2[1])<eps):
+        if internal(A4,A5,A6,A2)==1:
+            return 1
+    if (abs(mas2[2])<eps):
+        if internal(A4,A5,A6,A3)==1:
+            return 1
+    if (mas1[0]*mas1[1]<0) and (abs(mas1[0])>eps) and (abs(mas1[1])>eps):
+        t=intersect3(A1,A2,A3,A4,A5)
+        if (abs(t-0.5)<0.5+eps):
+            A7=[0]*3
+            for i in range(0,3):
+                A7[i]=A4[i]+t*A5[i]
+            if internal(A1,A2,A3,A7)==1:
+                return 1
+    if (mas1[0]*mas1[2]<0) and (abs(mas1[0])>eps) and (abs(mas2[1])>eps):
+        t=intersect3(A1,A2,A3,A4,A6)
+        if (abs(t-0.5)<0.5+eps):
+            A7=[0]*3
+            for i in range(0,3):
+                A7[i]=A4[i]+t*A6[i]
+            if internal(A1,A2,A3,A7)==1:
+                return 1
+    if (mas1[1]*mas1[2]<0) and (abs(mas1[2])>eps) and (abs(mas1[1])>eps):
+        t=intersect3(A1,A2,A3,A5,A6)
+        if (abs(t-0.5)<0.5+eps):
+            A7=[0]*3
+            for i in range(0,3):
+                A7[i]=A5[i]+t*A6[i]
+            if internal(A1,A2,A3,A7)==1:
+                return 1
+    if (mas2[0]*mas2[1]<0) and (abs(mas2[0])>eps) and (abs(mas2[1])>eps):
+        t=intersect3(A4,A5,A6,A1,A2)
+        if (abs(t-0.5)<0.5+eps):
+            A7=[0]*3
+            for i in range(0,3):
+                A7[i]=A1[i]+t*A2[i]
+            if internal(A4,A5,A6,A7)==1:
+                return 1
+    if (mas2[0]*mas2[2]<0) and (abs(mas2[0])>eps) and (abs(mas2[2])>eps):
+        t=intersect3(A4,A5,A6,A1,A3)
+        if (abs(t-0.5)<0.5+eps):
+            A7=[0]*3
+            for i in range(0,3):
+                A7[i]=A1[i]+t*A3[i]
+            if internal(A4,A5,A6,A7)==1:
+                return 1
+    if (mas2[1]*mas2[2]<0) and (abs(mas2[2])>eps) and (abs(mas2[1])>eps):
+        t=intersect3(A4,A5,A6,A2,A3)
+        if (abs(t-0.5)<0.5+eps):
+            A7=[0]*3
+            for i in range(0,3):
+                A7[i]=A2[i]+t*A3[i]
+            if internal(A4,A5,A6,A7)==1:
+                return 1
+    return 0
+lst=[]
+
+P1=[0,0,0]
+P2=[0,5,0]
+P3=[6,5,0]
+P4=[1,4,0]
+P5=[2,4,0]
+P6=[2,3,0]
+
+lst1=[]
+lst1.append(P1)
+lst1.append(P2)
+lst1.append(P3)
+lst1.append(P4)
+lst1.append(P5)
+lst1.append(P6)
+lst.append(lst1)
+
+P1=[-1,0,0]
+P2=[0,-1,0]
+P3=[0,0,0]
+P4=[0,0,0]
+P5=[0,3,0]
+P6=[5,0,0]
+lst1=[]
+lst1.append(P1)
+lst1.append(P2)
+lst1.append(P3)
+lst1.append(P4)
+lst1.append(P5)
+lst1.append(P6)
+lst.append(lst1)
+
+P1=[-1,0,0]
+P2=[0,2,0]
+P3=[0,0,0]
+P4=[0,0,0]
+P5=[5,0,0]
+P6=[0,4,0]
+lst1=[]
+lst1.append(P1)
+lst1.append(P2)
+lst1.append(P3)
+lst1.append(P4)
+lst1.append(P5)
+lst1.append(P6)
+lst.append(lst1)
+
+P1=[0,0,0]
+P2=[0,2,0]
+P3=[1,0,0]
+P4=[0,-1,0]
+P5=[0,3,0]
+P6=[7,-1,0]
+lst1=[]
+lst1.append(P1)
+lst1.append(P2)
+lst1.append(P3)
+lst1.append(P4)
+lst1.append(P5)
+lst1.append(P6)
+lst.append(lst1)
+
+P1=[0,0,0]
+P2=[0,4,0]
+P3=[4,0,0]
+P4=[1,2,0]
+P5=[1,1,-3]
+P6=[0.5,2,-2]
+lst1=[]
+lst1.append(P1)
+lst1.append(P2)
+lst1.append(P3)
+lst1.append(P4)
+lst1.append(P5)
+lst1.append(P6)
+lst.append(lst1)
+
+P1=[0,0,0]
+P2=[0,4,0]
+P3=[4,0,0]
+P4=[4,0,0]
+P5=[1,1,-3]
+P6=[0.5,2,-2]
+lst1=[]
+lst1.append(P1)
+lst1.append(P2)
+lst1.append(P3)
+lst1.append(P4)
+lst1.append(P5)
+lst1.append(P6)
+lst.append(lst1)
+
+P1=[0,0,0]
+P2=[0,4,0]
+P3=[4,0,0]
+P4=[1,2,0]
+P5=[2,1,0]
+P6=[0.5,2,-2]
+lst1=[]
+lst1.append(P1)
+lst1.append(P2)
+lst1.append(P3)
+lst1.append(P4)
+lst1.append(P5)
+lst1.append(P6)
+lst.append(lst1)
+
+P1=[0,0,0]
+P2=[0,4,0]
+P3=[4,0,0]
+P4=[-1,2,2]
+P5=[0,2,2]
+P6=[0,0,-2]
+lst1=[]
+lst1.append(P1)
+lst1.append(P2)
+lst1.append(P3)
+lst1.append(P4)
+lst1.append(P5)
+lst1.append(P6)
+lst.append(lst1)
+
+P1=[0,0,0]
+P2=[0,4,0]
+P3=[5,0,0]
+P4=[1,1,2]
+P5=[5,6,-2]
+P6=[3,-4,-1]
+lst1=[]
+lst1.append(P1)
+lst1.append(P2)
+lst1.append(P3)
+lst1.append(P4)
+lst1.append(P5)
+lst1.append(P6)
+lst.append(lst1)
+for j in range(0,len(lst)):
+    P1=lst[j][0]
+    P2=lst[j][1]
+    P3=lst[j][2]
+    P4=lst[j][3]
+    P5=lst[j][4]
+    P6=lst[j][5]
+    print(P1,P2,P3,P4,P5,P6)
+
+    res=intersect(P1,P2,P3,P4,P5,P6)
+    if res==1:
+        print("Треугольники пересекаются.")
+    elif res==-1:
+        print("Один из треугольников не является таковым")
+    else:
+        print("Треугольники не пересекаются.")
+
+    a = np.zeros((2, 9))
+    for i in range(0,3):
+        a[0,i]=P1[i]
+        a[0,i+3]=P2[i]
+        a[0,i+6]=P3[i]
+        a[1,i]=P4[i]
+        a[1,i+3]=P5[i]
+        a[1,i+6]=P6[i]
+    print(a)
+    
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    fc = ["crimson" if i%2 else "gold" for i in range(a.shape[0])]
+
+    poly3d = [[ a[i, j*3:j*3+3] for j in range(3)  ] for i in range(a.shape[0])]
+
+    ax.add_collection3d(Poly3DCollection(poly3d, facecolors=fc, linewidths=1))
+
+    ax.set_xlim(-a.max(),a.max())
+    ax.set_ylim(-a.max(),a.max())
+
+    plt.show()
---
+++ Task2410/Task7.py
@@ -0,0 +1,142 @@
+import scipy.integrate 
+import numpy as np
+import numpy.linalg as nlg
+import copy
+import matplotlib.pyplot as plt
+from threading import *
+import threading
+import time
+
+def f(y,t,lstm):
+    G=6.67 * 10 ** (-11)
+    N=int(len(y)/6)
+    mas=np.zeros(6*N)
+    for i in range(0,N):
+        mas[6*i:6*i+3]=y[6*i+3:6*i+6]
+        for j in range(0,N):
+            if i!=j:
+                mas[6*i+3:6*i+6]+=G*lstm[j]*(y[6*j:6*j+3]-y[6*i:6*i+3])/nlg.norm(y[6*j:6*j+3]-y[6*i:6*i+3],2) ** 3
+    return mas
+
+
+def calc1(lst,lst2,a,tau,j):
+    lst2[j,0:3]=lst[j,0:3]+lst[j,3:6]*tau+0.5*a*tau**2
+def calc2(lst,lst2,a,b,tau,j):
+    lst2[j,3:6]=lst[j,3:6]+0.5*(a+b)*tau
+def calc3(a,lst,lstm,i):
+    N=len(lst)
+    G=6.67 * 10 ** (-11)
+    for j in range(0,N):
+        if i!=j:
+            a[i]+=G*lstm[j]*(lst[j,0:3]-lst[i,0:3])/nlg.norm(lst[j,0:3]-lst[i,0:3],2) ** 3
+
+def acceleration(lst,lstm,i):
+    N=len(lst)
+    if (i==-1):
+        a=np.zeros((N,3))
+        for i in range(0,N):
+            calc3(a,lst,lstm,i)
+        return a
+    else:
+        a=np.zeros((N,3))
+        calc3(a,lst,lstm,i)
+        return a[i]
+def calc(res,lst,lst2,lstm,tau,j,M,evs,e):
+    N=len(lst)
+    a=acceleration(lst,lstm,j)
+    for i in range(1,M):
+        lst2[j]=np.zeros(6)
+        calc1(lst,lst2,a,tau,j)
+        evs[j].set()
+        e.wait()
+        e.clear()
+        b=acceleration(lst2,lstm,j)
+        calc2(lst,lst2,a,b,tau,j)
+        lst[j]=copy.copy(lst2[j])
+        a=copy.copy(b)
+        res[i,j]=copy.copy(lst[j])
+        evs[j].set()
+        e.wait()
+        e.clear()
+def bosswork(M,N,evs,e):
+    for i in range(1,M):
+        #time.sleep(10 ** -8 *M)
+        for ev in evs:
+            ev.wait()
+            ev.clear()
+        e.set()
+        for ev in evs:
+            ev.wait()
+            ev.clear()
+        e.set()
+def verlet(lst, lstm, M, T, type):
+    tau=T/M
+    N=len(lst)
+    res=np.zeros((M,N,6))
+    res[0]=copy.copy(lst)
+    if type=="verlet":
+        a=acceleration(lst,lstm,-1)
+        for i in range(1,M):
+            lst2=np.zeros((N,6))
+            for j in range(0,N):
+                calc1(lst,lst2,a[j],tau,j)
+            b=acceleration(lst2,lstm,-1)
+            for j in range(0,N):
+                calc2(lst,lst2,a[j],b[j],tau,j)
+            lst=copy.copy(lst2)
+            a=copy.copy(b)
+            res[i]=copy.copy(lst)
+    elif type=="verlet-threading":
+        e=threading.Event()
+        lst2=np.zeros((N,6))
+        evs=[]
+        for j in range(0,N):
+            ev=threading.Event()
+            evs.append(ev)
+        boss=Thread(target=bosswork,name="boss",args=(M,N,evs,e))
+        boss.start()
+        for j in range(0,N):
+            tn=Thread(target=calc,name="thread"+str(j),args=(res,lst,lst2,lstm,tau,j,M,evs,e))
+            tn.start()
+        boss.join()
+    elif type=="scipy":
+        t=np.linspace(0,T,M)
+        res2=scipy.integrate.odeint(f,lst.reshape((6*N)),t,args=(lstm,))
+        res=res2.reshape((M,N,6))
+    return res
+
+def sunandco(type="verlet"):
+    a=np.zeros(6)
+    b=np.zeros(6)
+    c=np.zeros(6)
+    a[1]=-384467000
+    a[3]=1022
+    a[4]=-29.783*10 **3
+    b[4]=-29.783*10 **3
+    c[0]=1.496*10 ** 11
+    #a[0]=1
+    #a[4]=1
+    #lst=[]
+    #lst.append(a)
+    #lst.append(b)
+    lst=np.zeros((3,6))
+    lst[0,:]=a
+    lst[1,:]=b
+    lst[2,:]=c
+    t=time.time()
+    lstm=[7.3 * 10 ** 22, 6*10 ** 24, 2* 10 **30 ]
+    #lstm=[1, 1*10 ** 11]
+    res = verlet(lst,lstm, 12000000, 12*2592000, type)
+    #res=verlet(lst,lstm,10000,1,"verlet-threading")
+    print(time.time()-t)
+    N=len(res)
+    print(N)
+    x1=res[:,0,0]
+    y1=res[:,0,1]
+    x2=res[:,1,0]
+    y2=res[:,1,1]
+    plt.plot(x1,y1)
+    plt.plot(x2,y2,color="red")
+    plt.show()
+
+sunandco("scipy")

121bfe7c37ed022b7e508e00ee6b9e474fbee0d7
dmitry.kharitonov <dmitrykharitonov@yandex.ru>
2017-10-29 20:01:54 +0300

    %sequences%

--- Task2609/Task1.py
+++ Task2609/Task1.py
@@ -2,23 +2,32 @@ import random as rnd
 lst=[];
 lst2=[];
 M=5;
-k=10;
-n=20;
+k=5;
+n=6;
 res=0;
 temp=0;
 for i in range(0,k):
     lst.append(rnd.randint(0,M))
 for i in range(0,n):
     lst2.append(rnd.randint(0,M))
-for i in range(0,k):
-    for j in range(0,n):
-        temp=0;
-        l=0;
-        while (i+l<k) and (j+l<n) and (lst[i+l]==lst2[j+l]):
-            temp+=1
-            l+=1
-        if (temp>res):
-            res=temp
+c=[]
+for i in range(0,k+1):
+    d=[0]*(n+1)
+    c.append(d)
+for i in range(0,k+1):
+    for j in range(0,n+1):
+        print(c[i][j],end=' ')
+    print()
+for i in range(1,k+1):
+    for j in range(1,n+1):
+        if lst[i-1]==lst2[j-1]:
+            c[i][j]=c[i-1][j-1]+1
+        else:
+            c[i][j]=max(c[i-1][j],c[i][j-1])
 print(lst)
 print(lst2)
-print(res)
+print(c[k][n])
+#for i in range(0,k+1):
+#    for j in range(0,n+1):
+#        print(c[i][j],end=' ')
+#    print()
--- Task2609/Task2.py
+++ Task2609/Task2.py
@@ -1,34 +1,46 @@
 import random as rnd
+import math
 lst=[];
 lst2=[];
-M=5;
+M=50;
 k=10;
 res=1;
 temp=1;
 for i in range(0,k):
     lst.append(rnd.randint(0,M))
 i=0
-lst[0]=0.5
+#lst[0]=0.5
+print(lst)
 try:
-    if not isinstance(lst[0],int):
-        raise TypeError("Неправильный тип")
-    while (i<k-1):
-        if not isinstance(lst[i+1],int):
+    P=[0]*k
+    M=[0]*(k+1)
+    L=0
+    for i in range(0,k):
+        if not isinstance(lst[i],int):
             raise TypeError("Неправильный тип")
-        l=0
-        temp=1
-        while (i+l<k-1) and (lst[i+l]<lst[i+l+1]):
-            temp+=1
-            l+=1
-        if (temp>res):
-            res=temp
-        i+=l+1
+        lo=1
+        hi=L
+        while (lo<=hi):
+            mid=math.ceil((lo+hi)/2)
+            if (lst[M[mid]]<lst[i]):
+                lo=mid+1
+            else:
+                hi=mid-1
+        newl=lo
+        P[i]=M[newl-1]
+        M[newl]=i
+        if newl>L:
+            L=newl
+        #print(P)
+    S=[0]*L
+    k=M[L]
+    for i in range(L-1,-1,-1):
+        S[i]=lst[k]
+        k=P[k]
+    print(L)
+    print(S)
 except (TypeError) as inst:
     print(type(inst))
     print(inst)
     res=0
-finally:
-    print(lst)
-    if (res>0):
-        print(res)
 
